0e395923ffcd89ac6d7e05d93afce001c2a2a012
hyracinth <jonahlou@gmail.com>
2020-12-09 12:45:07 -0500

    Solved day 9

--- AdventOfCode2020/Solutions/Day09.cs
+++ AdventOfCode2020/Solutions/Day09.cs
@@ -13,13 +13,96 @@ namespace AdventOfCode2020.Solutions
     {
         public string SolveP1()
         {
-            string[] inputsRaw = AoCUtils.readInputFile(this.GetType().Name.Substring(3));
-            return null;
+            long[] inputsRaw = AoCUtils.readInputFile(this.GetType().Name.Substring(3)).ToList().Select(x => long.Parse(x)).ToArray();
+            int preambleCount = 25;
+            long invalidNum = long.MinValue;
+
+            for(int ii = preambleCount - 1; ii < inputsRaw.Length - 1; ii++)
+            {
+                Boolean validNext = false;
+                for(int jj = 0; jj < preambleCount; jj++)
+                {
+                    if(inputsRaw[ii - jj] > inputsRaw[ii + 1])
+                    {
+                        continue;
+                    }
+
+                    for(int kk = 1; kk < preambleCount; kk++)
+                    {
+                        if (jj == kk || inputsRaw[ii - kk] > inputsRaw[ii + 1])
+                        {
+                            continue;
+                        }
+
+                        if(inputsRaw[ii - jj] + inputsRaw[ii - kk] == inputsRaw[ii + 1])
+                        {
+                            validNext = true;
+                            break;
+                        }
+                    }
+
+                    if(validNext)
+                    {
+                        break;
+                    }
+                }
+                
+                if(!validNext)
+                {
+                    invalidNum = inputsRaw[ii + 1];
+                    break;
+                }
+            }
+
+            return invalidNum.ToString();
         }
 
         public string SolveP2()
         {
-            return null;
+            long[] inputsRaw = AoCUtils.readInputFile(this.GetType().Name.Substring(3)).ToList().Select(x => long.Parse(x)).ToArray();
+            int invalidId = 539;
+            long invalidNum = inputsRaw[invalidId];
+
+            Boolean foundWeakness = false;
+            long runningSum = 0;
+            long runningCount = 0;
+            int lastId = 0;
+            List<long> weaknessList = new List<long>();
+
+            for (int ii = 0; ii < inputsRaw.Length; ii++)
+            {
+                if(foundWeakness)
+                {
+                    break;
+                }
+                runningSum = 0;
+                runningCount = 0;
+                for(int jj = ii; jj < inputsRaw.Length; jj++)
+                {
+                    if (runningSum > invalidNum)
+                    {
+                        break;
+                    }
+                    runningSum += inputsRaw[jj];
+                    runningCount++;
+
+                    if (runningSum == invalidNum)
+                    {
+                        lastId = jj;
+                        foundWeakness = true;
+                        break;
+                    }
+                }
+            }
+
+            for(int ii = 0; ii < runningCount; ii++)
+            {
+                weaknessList.Add(inputsRaw[lastId - ii]);
+            }
+
+            long weakness = weaknessList.Min() + weaknessList.Max();
+
+            return weakness.ToString();
         }
 
         public string SolveP3()
